66ee16dd379c11dfaf6d6aefc3458910c185e1fd
danielkoren <daniels.korens@gmail.com>
2020-12-31 15:26:53 +0100

    Add routes + basic controllers + views for home & about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'               # routes root (website.com/) to "pages" controller and "home" action
+  get 'about', to: 'pages#about'  # routes HTTP GET to website.com/about to "pages" controller and "about" action
+
 end
+
