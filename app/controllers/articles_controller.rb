8ed7d023e31c5d485835e2ae7b7959d980e1c964
danielkoren <daniels.korens@gmail.com>
2021-01-23 20:53:30 +0100

    Add validation notice and flash msgs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,28 @@
 class ArticlesController < ApplicationController
 
-
-    # defining a show action
-
     def show 
         @article = Article.find(params[:id])
     end
     
     def index
         @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        
+        if @article.save
+            
+            flash[:notice] = "Article created successfully!"
+            redirect_to @article
+            
+        else
+        
+            render 'new'
+        end
     end
 end
