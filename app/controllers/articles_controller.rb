c7b5da063ae112633175243bf37bb4ca761ff7ff
danielkoren <daniels.korens@gmail.com>
2021-01-24 15:01:47 +0100

    Add article edit action/view/routes for get+patch

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         
@@ -23,6 +27,20 @@ class ArticlesController < ApplicationController
         else
         
             render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            
+            flash[:notice] = "Article updated successfully!"
+            redirect_to @article
+        
+        else
+        
+            render 'edit'
+
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'               # routes root (website.com/) to "pages" controller and "home" action
   get 'about', to: 'pages#about'  # routes HTTP GET to website.com/about to "pages" controller and "about" action
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 
 
 end
